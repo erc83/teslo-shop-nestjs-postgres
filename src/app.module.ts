c9f10b66bd79409dae13e14e8973d57c900ef793
erc83 <espinoza.eric@gmail.com>
2024-11-24 00:10:50 -0300

    add: config, variables entorno and conexcion DB

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,24 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [],
+  imports: [ 
+    ConfigModule.forRoot(),         //variables entorno
+
+    TypeOrmModule.forRoot({         // un forRoot mas son feature
+      type: 'postgres',
+      host: process.env.DB_HOST,
+      port: +process.env.DB_PORT,       // +number    se tranforma en numero 
+      database: process.env.DB_NAME,
+      username: process.env.DB_USERNAME,
+      password: process.env.DB_PASSWORD,
+      autoLoadEntities: true,         // cargar entidades que se definan poco a poco
+      synchronize: true,              // al hacer cambio en la entidades, automaticamente las sincroniza
+    }),        
+  ], 
   controllers: [AppController],
   providers: [AppService],
 })
