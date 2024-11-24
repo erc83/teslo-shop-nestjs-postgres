687175db1497319d2287c5542da943b89cd57df7
erc83 <espinoza.eric@gmail.com>
2024-11-24 00:52:18 -0300

    upd: columns entity products

--- src/products/entities/product.entity.ts
+++ src/products/entities/product.entity.ts
@@ -5,9 +5,35 @@ import { Entity, PrimaryGeneratedColumn, Column} from "typeorm";
 export class Product {
 
     @PrimaryGeneratedColumn('uuid')
-    id: string                         // defecto number
+    id: string;                         // defecto number
 
     @Column('text', { unique: true,} )
-    title: string
+    title: string;
+    
+    @Column('numeric', { default: 0 })
+    price: number;
+
+    // otra forma de crear la columna
+    @Column({
+        type: 'text',
+        nullable: true
+    })
+    description: string;
+    
+    @Column('text', { unique: true })
+    slug: string;
+    
+    @Column('int', { default: 0 })
+    stock: number;
+
+    // arreglo de string
+    @Column('text', { array: true })
+    sizes: string[];
+
+    @Column('text')
+    gender: string;
+
+    //tags
+    //images
 
 }
