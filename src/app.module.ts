9d9fe2e7386e563c3f4ffee03fd38344a14e91e7
erc83 <espinoza.eric@gmail.com>
2024-12-24 00:13:53 -0300

    add: create modulo auth initial

--- src/app.module.ts
+++ src/app.module.ts
@@ -11,6 +11,7 @@ import { ProductsModule } from './products/products.module';
 import { CommonModule } from './common/common.module';
 import { SeedModule } from './seed/seed.module';
 import { FilesModule } from './files/files.module';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
   imports: [ 
@@ -25,11 +26,14 @@ import { FilesModule } from './files/files.module';
       password: process.env.DB_PASSWORD,
       autoLoadEntities: true,         // cargar entidades que se definan poco a poco
       synchronize: true,              // al hacer cambio en la entidades, automaticamente las sincroniza
-    }), ProductsModule, CommonModule, 
+    }), 
+    ProductsModule, 
+    CommonModule, 
     FilesModule,        
     ServeStaticModule.forRoot({
       rootPath: join(__dirname, '..', '/public'),       // http://localhost:3000/assets/img/1473809-00-A_1_2000.jpg
-    }),
+    }), 
+    AuthModule,
   ], 
 
   controllers: [AppController],
