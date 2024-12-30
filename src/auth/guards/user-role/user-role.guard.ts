0429791706d82fcfac5b1b49eab0a775d924878c
erc83 <espinoza.eric@gmail.com>
2024-12-30 01:34:06 -0300

    add: validRoles in user-role.guard.ts

--- src/auth/guards/user-role/user-role.guard.ts
+++ src/auth/guards/user-role/user-role.guard.ts
@@ -1,17 +1,23 @@
-import { BadRequestException, CanActivate, ExecutionContext, Injectable } from '@nestjs/common';
+import { Reflector } from '@nestjs/core';
+import { BadRequestException, CanActivate, ExecutionContext, Injectable } 
+  from '@nestjs/common';
 import { Observable } from 'rxjs';
 
 @Injectable()
 export class UserRoleGuard implements CanActivate {
+
+  constructor(
+    private readonly reflector: Reflector,
+  ) {}
 
   // para que el guard sea valido tiene que implementar el canActivate que devuelve un valor booleano
   canActivate(
     context: ExecutionContext,
   ): boolean | Promise<boolean> | Observable<boolean> {
 
-    console.log('UserRoleGuard')
+    const validRoles : string[] = this.reflector.get( 'roles', context.getHandler() )
     
-    //throw new BadRequestException("Provando excepcion con el Guard")
+    console.log({ validRoles })
 
     return true;      // si retorna false no entra en la ruta privada2
   }
